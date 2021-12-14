0dfd4e4fcdea1583b5f02dff343f852141491bd9
damienbod <damien_bod@hotmail.com>
2021-12-14 11:44:00 +0100

    Update clients

---
+++ PresenceClient/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using PresenceClient.Presence;
+using System.Text.Json;
+using System.Collections.Generic;
+using Microsoft.Graph;
+
+namespace GraphEmailClient
+{
+    public partial class MainWindow : Window
+    {
+        AadGraphApiDelegatedClient _aadGraphApiDelegatedClient = new AadGraphApiDelegatedClient();
+
+        const string SignInString = "Sign In";
+        const string ClearCacheString = "Clear Cache";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _aadGraphApiDelegatedClient.InitClient();
+        }
+
+        private async void SignIn(object sender = null, RoutedEventArgs args = null)
+        {
+            var accounts = await _aadGraphApiDelegatedClient.GetAccountsAsync();
+
+            if (SignInButton.Content.ToString() == ClearCacheString)
+            {
+                await _aadGraphApiDelegatedClient.RemoveAccountsAsync();
+
+                SignInButton.Content = SignInString;
+                UserName.Content = "Not signed in";
+                return;
+            }
+
+            try
+            {
+                var account = await _aadGraphApiDelegatedClient.SignIn();
+
+                Dispatcher.Invoke(() =>
+                {
+                    SignInButton.Content = ClearCacheString;
+                    SetUserName(account);
+                });
+            }
+            catch (MsalException ex)
+            {
+                if (ex.ErrorCode == "access_denied")
+                {
+                    // The user canceled sign in, take no action.
+                }
+                else
+                {
+                    // An unexpected error occurred.
+                    string message = ex.Message;
+                    if (ex.InnerException != null)
+                    {
+                        message += "Error Code: " + ex.ErrorCode + "Inner Exception : " + ex.InnerException.Message;
+                    }
+
+                    MessageBox.Show(message);
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    UserName.Content = "Not signed in";
+                });
+            }
+        }
+
+        private async void GetPresenceforEmail(object sender, RoutedEventArgs e)
+        {
+            var cloudCommunicationPages = await _aadGraphApiDelegatedClient.GetPresenceAsync(EmailRecipientText.Text);
+
+            var allPresenceItems = new List<Presence>();
+
+            while (cloudCommunicationPages != null && cloudCommunicationPages.Count > 0)
+            {
+                foreach (var presence in cloudCommunicationPages)
+                {
+                    allPresenceItems.Add(presence);
+                }
+                //check if next result page is available
+                if (cloudCommunicationPages.NextPageRequest == null)
+                    break;
+            }
+
+            EmailBody.Text = JsonSerializer.Serialize(allPresenceItems);
+        }
+
+
+        private void SetUserName(IAccount userInfo)
+        {
+            string userName = null;
+
+            if (userInfo != null)
+            {
+                userName = userInfo.Username;
+            }
+
+            if (userName == null)
+            {
+                userName = "Not identified";
+            }
+
+            UserName.Content = userName;
+        }
+    }
+}
---
+++ PresenceClient/Presence/AadGraphApiDelegatedClient.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using GraphEmailClient;
+
+namespace PresenceClient.Presence
+{
+    public class AadGraphApiDelegatedClient
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private IPublicClientApplication _app;
+
+        private static readonly string AadInstance = ConfigurationManager.AppSettings["AADInstance"];
+        private static readonly string Tenant = ConfigurationManager.AppSettings["Tenant"];
+        private static readonly string ClientId = ConfigurationManager.AppSettings["ClientId"];
+        private static readonly string Scope = ConfigurationManager.AppSettings["Scope"];
+
+        private static readonly string Authority = string.Format(CultureInfo.InvariantCulture, AadInstance, Tenant);
+        private static readonly string[] Scopes = { Scope };
+
+        public void InitClient()
+        {
+            _app = PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(Authority)
+                .WithRedirectUri("http://localhost:65419") // needed only for the system browser
+                .Build();
+
+            TokenCacheHelper.EnableSerialization(_app.UserTokenCache);
+        }
+
+        public async Task<IAccount> SignIn()
+        {
+            try
+            {
+                var result = await AcquireTokenSilent();
+                return result.Account;
+            }
+            catch (MsalUiRequiredException)
+            {
+                return await AcquireTokenInteractive().ConfigureAwait(false);
+            }
+        }
+
+        private async Task<IAccount> AcquireTokenInteractive()
+        {
+            var accounts = (await _app.GetAccountsAsync()).ToList();
+
+            var builder = _app.AcquireTokenInteractive(Scopes)
+                .WithAccount(accounts.FirstOrDefault())
+                .WithUseEmbeddedWebView(false)
+                .WithPrompt(Microsoft.Identity.Client.Prompt.SelectAccount);
+
+            var result = await builder.ExecuteAsync().ConfigureAwait(false);
+
+            return result.Account;
+        }
+
+        public async Task<AuthenticationResult> AcquireTokenSilent()
+        {
+            var accounts = await GetAccountsAsync();
+            var result = await _app.AcquireTokenSilent(Scopes, accounts.FirstOrDefault())
+                    .ExecuteAsync()
+                    .ConfigureAwait(false);
+
+            return result;
+        }
+
+        public async Task<IList<IAccount>> GetAccountsAsync()
+        {
+            var accounts = await _app.GetAccountsAsync();
+            return accounts.ToList();
+        }
+
+        public async Task RemoveAccountsAsync()
+        {
+            IList<IAccount> accounts = await GetAccountsAsync();
+
+            // Clears the library cache. Does not affect the browser cookies.
+            while (accounts.Any())
+            {
+                await _app.RemoveAsync(accounts.First());
+                accounts = await GetAccountsAsync();
+            }
+        }
+
+        public async Task<ICloudCommunicationsGetPresencesByUserIdCollectionPage> GetPresenceAsync(string email)
+        {
+            var result = await AcquireTokenSilent();
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var graphClient = new GraphServiceClient(_httpClient)
+            {
+                AuthenticationProvider = new DelegateAuthenticationProvider(async (requestMessage) =>
+                {
+                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                    await Task.FromResult<object>(null);
+                })
+            };
+
+            var upn = await GetUserIdAsync(graphClient, email);
+            var ids = new List<string>()
+            {
+                upn
+            };
+
+            return await graphClient.Communications
+                .GetPresencesByUserId(ids)
+                .Request()
+                .PostAsync();
+        }
+
+        private async Task<string> GetUserIdAsync(GraphServiceClient graphServiceClient, string email)
+        {
+
+            var filter = $"startswith(userPrincipalName,'{email}')";
+
+            var users = await graphServiceClient.Users
+                .Request()
+                .Filter(filter)
+                .GetAsync();
+
+            return users.CurrentPage[0].Id;
+        }
+    }
+}
