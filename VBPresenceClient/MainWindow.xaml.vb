8ec1b5602b063946946d3cb7a6e6a1ec37d2b3fe
damienbod <damien_bod@hotmail.com>
2021-12-14 17:02:19 +0100

    Adding VB presence

---
+++ VBPresenceClient/MainWindow.xaml.vb
@@ -0,0 +1,84 @@
+﻿Imports Microsoft.Identity.Client
+Imports VBPresenceClient.PresenceClient.Presence
+Imports System.Text.Json
+Imports Microsoft.Graph
+
+
+Partial Public Class MainWindow
+    Inherits Window
+
+    Private _aadGraphApiDelegatedClient As AadGraphApiDelegatedClient = New AadGraphApiDelegatedClient()
+    Const SignInString As String = "Sign In"
+    Const ClearCacheString As String = "Clear Cache"
+
+    Public Sub New()
+        InitializeComponent()
+        _aadGraphApiDelegatedClient.InitClient()
+    End Sub
+
+    Private Async Sub SignIn(ByVal Optional sender As Object = Nothing, ByVal Optional args As RoutedEventArgs = Nothing)
+        Dim accounts = Await _aadGraphApiDelegatedClient.GetAccountsAsync()
+
+        If SignInButton.Content.ToString() = ClearCacheString Then
+            Await _aadGraphApiDelegatedClient.RemoveAccountsAsync()
+            SignInButton.Content = SignInString
+            UserName.Content = "Not signed in"
+            Return
+        End If
+
+        Try
+            Dim account = Await _aadGraphApiDelegatedClient.SignIn()
+            Dispatcher.Invoke(Function()
+                                  SignInButton.Content = ClearCacheString
+                                  SetUserName(account)
+                              End Function)
+        Catch ex As MsalException
+
+            If ex.ErrorCode = "access_denied" Then
+            Else
+                Dim message As String = ex.Message
+
+                If ex.InnerException IsNot Nothing Then
+                    message += "Error Code: " & ex.ErrorCode & "Inner Exception : " + ex.InnerException.Message
+                End If
+
+                MessageBox.Show(message)
+            End If
+
+            Dispatcher.Invoke(Function()
+                                  UserName.Content = "Not signed in"
+                              End Function)
+        End Try
+    End Sub
+
+    Private Async Sub GetPresenceforEmail(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Dim cloudCommunicationPages = Await _aadGraphApiDelegatedClient.GetPresenceAsync(EmailRecipientText.Text)
+        Dim allPresenceItems = New List(Of Presence)()
+
+        While cloudCommunicationPages IsNot Nothing AndAlso cloudCommunicationPages.Count > 0
+
+            For Each presence In cloudCommunicationPages
+                allPresenceItems.Add(presence)
+            Next
+
+            If cloudCommunicationPages.NextPageRequest Is Nothing Then Exit While
+        End While
+
+        EmailBody.Text = JsonSerializer.Serialize(allPresenceItems)
+    End Sub
+
+    Private Sub SetUserName(ByVal userInfo As IAccount)
+        Dim userNameString As String = Nothing
+
+        If userInfo IsNot Nothing Then
+            userNameString = userInfo.Username
+        End If
+
+        If userNameString Is Nothing Then
+            userNameString = "Not identified"
+        End If
+
+        UserName.Content = userNameString
+    End Sub
+End Class
+
---
+++ VBPresenceClient/Presence/AadGraphApiDelegatedClient.vb
@@ -0,0 +1,87 @@
+﻿Imports Microsoft.Graph
+Imports Microsoft.Identity.Client
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Globalization
+Imports System.Linq
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Threading.Tasks
+Imports GraphEmailClient
+
+Namespace PresenceClient.Presence
+    Public Class AadGraphApiDelegatedClient
+        Private ReadOnly _httpClient As HttpClient = New HttpClient()
+        Private _app As IPublicClientApplication
+        Private Shared ReadOnly AadInstance As String = ConfigurationManager.AppSettings("AADInstance")
+        Private Shared ReadOnly Tenant As String = ConfigurationManager.AppSettings("Tenant")
+        Private Shared ReadOnly ClientId As String = ConfigurationManager.AppSettings("ClientId")
+        Private Shared ReadOnly Scope As String = ConfigurationManager.AppSettings("Scope")
+        Private Shared ReadOnly Authority As String = String.Format(CultureInfo.InvariantCulture, AadInstance, Tenant)
+        Private Shared ReadOnly Scopes As String() = {Scope}
+
+        Public Sub InitClient()
+            _app = PublicClientApplicationBuilder.Create(ClientId).WithAuthority(Authority).WithRedirectUri("http://localhost:65419").Build()
+            TokenCacheHelper.EnableSerialization(_app.UserTokenCache)
+        End Sub
+
+        Public Async Function SignIn() As Task(Of IAccount)
+            Try
+                Dim result = Await AcquireTokenSilent()
+                Return result.Account
+            Catch __unusedMsalUiRequiredException1__ As MsalUiRequiredException
+                Return AcquireTokenInteractive().GetAwaiter().GetResult()
+            End Try
+        End Function
+
+        Private Async Function AcquireTokenInteractive() As Task(Of IAccount)
+            Dim accounts = (Await _app.GetAccountsAsync()).ToList()
+            Dim builder = _app.AcquireTokenInteractive(Scopes).WithAccount(accounts.FirstOrDefault()).WithUseEmbeddedWebView(False).WithPrompt(Microsoft.Identity.Client.Prompt.SelectAccount)
+            Dim result = Await builder.ExecuteAsync().ConfigureAwait(False)
+            Return result.Account
+        End Function
+
+        Public Async Function AcquireTokenSilent() As Task(Of AuthenticationResult)
+            Dim accounts = Await GetAccountsAsync()
+            Dim result = Await _app.AcquireTokenSilent(Scopes, accounts.FirstOrDefault()).ExecuteAsync().ConfigureAwait(False)
+            Return result
+        End Function
+
+        Public Async Function GetAccountsAsync() As Task(Of IList(Of IAccount))
+            Dim accounts = Await _app.GetAccountsAsync()
+            Return accounts.ToList()
+        End Function
+
+        Public Async Function RemoveAccountsAsync() As Task
+            Dim accounts As IList(Of IAccount) = Await GetAccountsAsync()
+
+            While accounts.Any()
+                Await _app.RemoveAsync(accounts.First())
+                accounts = Await GetAccountsAsync()
+            End While
+        End Function
+
+        Public Async Function GetPresenceAsync(ByVal email As String) As Task(Of ICloudCommunicationsGetPresencesByUserIdCollectionPage)
+            Dim result = Await AcquireTokenSilent()
+            _httpClient.DefaultRequestHeaders.Authorization = New AuthenticationHeaderValue("Bearer", result.AccessToken)
+            _httpClient.DefaultRequestHeaders.Accept.Add(New MediaTypeWithQualityHeaderValue("application/json"))
+            Dim graphClient = New GraphServiceClient(_httpClient) With {
+                .AuthenticationProvider = New DelegateAuthenticationProvider(Async Function(requestMessage)
+                                                                                 requestMessage.Headers.Authorization = New AuthenticationHeaderValue("Bearer", result.AccessToken)
+                                                                                 Await Task.FromResult(Of Object)(Nothing)
+                                                                             End Function)
+            }
+            Dim upn = Await GetUserIdAsync(graphClient, email)
+            Dim ids = New List(Of String)() From {
+                upn
+            }
+            Return Await graphClient.Communications.GetPresencesByUserId(ids).Request().PostAsync()
+        End Function
+
+        Private Async Function GetUserIdAsync(ByVal graphServiceClient As GraphServiceClient, ByVal email As String) As Task(Of String)
+            Dim filter = $"startswith(userPrincipalName,'{email}')"
+            Dim users = Await graphServiceClient.Users.Request().Filter(filter).GetAsync()
+            Return users.CurrentPage(0).Id
+        End Function
+    End Class
+End Namespace
